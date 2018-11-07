b36c6d8e9938579522464259fdcd4a7f765b54bc
Truong Giang <truongwp@gmail.com>
2018-11-07 23:30:09 +0700

    Fix wrong total rating calculation #104
    Fixes #104

--- includes/functions.php
+++ includes/functions.php
@@ -3079,7 +3079,7 @@ function wp_review_visitor_rate( $post_id, $rating_data ) {
 					$output['html']    = wp_review_get_review_box( $post_id );
 					$output['reviews'] = $reviews;
 				} else {
-					$post_reviews           = mts_get_post_reviews( $post_id );
+					$post_reviews           = mts_get_post_reviews( $post_id, true );
 					$output['html']         = wp_review_rating( $post_reviews['rating'], $post_id, array( 'user_rating' => true ) );
 					$output['rating_total'] = $post_reviews['rating'];
 					$output['rating_count'] = $post_reviews['count'];
