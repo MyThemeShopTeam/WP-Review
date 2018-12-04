a6458cf1a3f338c49571a307ae597ee80717a989
Truong G <truong.g@mythemeshop.com>
2018-12-04 22:52:57 +0700

    Remove sourcemaps from css and js #109
    Fixes #109

--- gulpfile.js
+++ gulpfile.js
@@ -11,20 +11,20 @@ const sassOptions = {
 gulp.task('sass', function() {
 	return gulp
 		.src('public/sass/*.scss')
-		.pipe(sourcemaps.init())
+		// .pipe(sourcemaps.init())
 		.pipe(sass(sassOptions).on('error', sass.logError))
 		.pipe(autoprefixer())
-		.pipe(sourcemaps.write('../sourcemaps'))
+		// .pipe(sourcemaps.write('../sourcemaps'))
 		.pipe(gulp.dest('public/css'));
 });
 
 gulp.task('admin-sass', function() {
 	return gulp
 		.src('admin/assets/sass/*.scss')
-		.pipe(sourcemaps.init())
+		// .pipe(sourcemaps.init())
 		.pipe(sass(sassOptions).on('error', sass.logError))
 		.pipe(autoprefixer())
-		.pipe(sourcemaps.write('../sourcemaps'))
+		// .pipe(sourcemaps.write('../sourcemaps'))
 		.pipe(gulp.dest('admin/assets/css'));
 });
 
