46636c80e6df5cbbdcc938e75fe211e7e6197839
Truong G <truong.g@mythemeshop.com>
2019-01-26 22:44:03 +0700

    Remove magnificpopup scripts from frontend #118
    Fixes #118

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -20,14 +20,11 @@ add_action( 'wp_enqueue_scripts', 'wp_review_enqueue', 12 );
  */
 function wp_review_enqueue() {
 	// Register.
-	wp_register_style( 'magnificPopup', WP_REVIEW_ASSETS . 'css/magnific-popup.css', array(), '1.1.0' );
-	wp_register_script( 'magnificPopup', WP_REVIEW_ASSETS . 'js/jquery.magnific-popup.min.js', array( 'jquery' ), '1.1.0', true );
-
 	wp_register_style( 'wp_review-style', WP_REVIEW_ASSETS . 'css/wp-review.css', array(), WP_REVIEW_PLUGIN_VERSION, 'all' );
 
 	wp_register_script( 'js-cookie', WP_REVIEW_ASSETS . 'js/js.cookie.min.js', array(), '2.1.4', true );
 
-	wp_register_script( 'wp_review-js', WP_REVIEW_ASSETS . 'js/main.js', array( 'magnificPopup', 'js-cookie', 'wp-util' ), WP_REVIEW_PLUGIN_VERSION, true );
+	wp_register_script( 'wp_review-js', WP_REVIEW_ASSETS . 'js/main.js', array( 'js-cookie', 'wp-util' ), WP_REVIEW_PLUGIN_VERSION, true );
 
 	wp_localize_script(
 		'wp_review-js',
@@ -43,8 +40,6 @@ function wp_review_enqueue() {
 	wp_enqueue_script( 'js-cookie' );
 	wp_enqueue_script( 'wp_review-js' );
 	wp_enqueue_script( 'wp_review-jquery-appear' );
-	wp_enqueue_style( 'magnificPopup' );
-	wp_enqueue_script( 'magnificPopup' );
 	wp_enqueue_style( 'wp_review-style' );
 }
 
