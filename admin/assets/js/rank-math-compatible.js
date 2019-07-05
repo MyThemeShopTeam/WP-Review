a1fdd3b117e66e40f4387480ed9f91dcf5a04166
pratikmts <pratik.d@mythemeshop.com>
2019-07-05 12:34:43 +0530

    Separated the functions
    Separated the functions

--- admin/assets/js/rank-math-compatible.js
+++ admin/assets/js/rank-math-compatible.js
@@ -1,29 +1,68 @@
 /* global RankMath */
 $( window ).on( 'load', function() {
+	var rankMathCompatible = function() {
+		this.init();
+		this.hooks();
+		this.events( this );
+	}
 
-  rankMathCompatible = function() {
-    RankMath.app.registerPlugin( 'rank-math-review-analysis', { status: 'ready' } );
-    RankMath.app.addFilter( 'content', this.reviewDescription, 'rank-math-review-analysis' );
-    RankMath.app.addFilter( 'title', this.reviewTitle, 'rank-math-review-analysis' );
-
-    this.events();
+	rankMathCompatible.prototype.init = function() {
+    this.pluginName = 'rank-math-review-analysis';
+    this.fields = {
+      content: {
+        'wp_review_desc' : 'editor'
+      },
+      'title' : {
+        'wp_review_desc_title' : 'text',
+      }
+    };
   }
 
-  // Analyze Review fields content.
-  rankMathCompatible.prototype.reviewDescription = function( content ) {
-    return content + tinyMCE.editors['wp_review_desc'].getContent();
-  };
+  rankMathCompatible.prototype.hooks = function() {
+  	RankMath.app.registerPlugin( this.pluginName, { status: 'ready' } );
+		RankMath.app.addFilter( 'content', $.proxy( this.reviewDescription, this ), this.pluginName );
+		RankMath.app.addFilter( 'title', $.proxy( this.reviewTitle, this ), this.pluginName );
+  }
 
-  // Analyze Review fields title.
-  rankMathCompatible.prototype.reviewTitle = function( title ) {
-    return title + $( '#wp_review_desc_title' ).val();
-  };
-
-  rankMathCompatible.prototype.events = function(data) {
-    tinyMCE.editors.wp_review_desc.on( 'change', function() {
-      RankMath.app.refresh()
+  rankMathCompatible.prototype.getContent = function() {
+    var content = '';
+    $.each( this.fields.content, function( key, value ) {
+      content += 'editor' === value && undefined !== tinyMCE.editors[key] ? tinyMCE.editors[key].getContent() : $( '#' + key ).val();
     });
-  };
+    return content;
+  }
 
-  new rankMathCompatible();
+  rankMathCompatible.prototype.getTitle = function() {
+    var title = '';
+    $.each( this.fields.title, function( key, value ) {
+      title = '\n' + $( '#' + key ).val();
+    });
+    return title;
+  }
+
+	// Analyze Review fields content.
+	rankMathCompatible.prototype.reviewDescription = function( content ) {
+		return content + this.getContent();
+	};
+
+	// Analyze Review fields title.
+	rankMathCompatible.prototype.reviewTitle = function( title ) {
+		return title + this.getTitle();
+	};
+
+	rankMathCompatible.prototype.events = function( self ) {
+		$.each( self.fields.content, function( key, value ) {
+			if ( 'editor' === value && undefined !== tinyMCE.editors[key] ) {
+				tinyMCE.editors[key].on( 'change', function() {
+      		RankMath.app.pluginReloaded( self.pluginName );
+				});
+			}
+      $( '#' + key ).on( 'change', function() {
+      	RankMath.app.pluginReloaded( self.pluginName );
+      });
+    });
+
+	};
+
+	new rankMathCompatible();
 });
