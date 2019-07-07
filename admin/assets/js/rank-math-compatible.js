4b7b49d2f93f7666a140b86ee2f826b07d4a552c
Shakeeb Ahmed <me@shakeebahmed.com>
2019-07-07 19:39:31 +0500

    Update rank-math-compatible.js

--- admin/assets/js/rank-math-compatible.js
+++ admin/assets/js/rank-math-compatible.js
@@ -1,83 +1,110 @@
-/* global RankMathApp */
-var rankMathCompatible = function() {
-	this.init();
-	this.hooks();
-  setTimeout( this.events.bind( this ), 1500 )
-}
+/* global RankMath */
+;( function( $ ) {
 
-rankMathCompatible.prototype.init = function() {
-  this.pluginName = 'rank-math-review-analysis';
-  this.fields = {
-    content: {
-      'wp_review_desc' : 'editor'
-    },
-    'title' : {
-      'wp_review_desc_title' : 'text',
-    }
-  };
-}
+	/**
+	 * RankMath integration class.
+	 */
+	var RankMathIntegration = function() {
+		this.init()
+		this.hooks()
+		this.events()
+	}
 
-rankMathCompatible.prototype.hooks = function() {
-	RankMathApp.registerPlugin( this.pluginName );
-	wp.hooks.addFilter( 'rank_math_content', this.pluginName, $.proxy( this.reviewDescription, this ) );
-  wp.hooks.addFilter( 'rank_math_title', this.pluginName, $.proxy( this.reviewTitle, this ) );
-}
-
-rankMathCompatible.prototype.getContent = function() {
-  var content = '';
-  $.each( this.fields.content, function( key, value ) {
-    content += 'editor' === value && undefined !== tinyMCE.editors[key] ? tinyMCE.editors[key].getContent() : $( '#' + key ).val();
-  });
-  return content;
-}
-
-rankMathCompatible.prototype.getTitle = function() {
-  var title = '';
-  $.each( this.fields.title, function( key, value ) {
-    title = '\n' + $( '#' + key ).val();
-  });
-  return title;
-}
-
-// Analyze Review fields content.
-rankMathCompatible.prototype.reviewDescription = function( content ) {
-	return content + this.getContent();
-};
-
-// Analyze Review fields title.
-rankMathCompatible.prototype.reviewTitle = function( title ) {
-	return title + this.getTitle();
-};
-
-rankMathCompatible.prototype.events = function() {
-  var self = this
-	$.each( this.fields.content, function( key, value ) {
-		if ( 'editor' === value && undefined !== tinyMCE.editors[key] ) {
-      tinyMCE.editors[key].on( 'keyup change', self.debounce( ( event ) => {
-        RankMathApp.reloadPlugin( self.pluginName );
-      }, 500 ));
+	/**
+	 * Init the plugin
+	 */
+	RankMathIntegration.prototype.init = function() {
+		this.pluginName = 'rank-math-review-analysis'
+		this.fields     = {
+			content: {
+				'wp_review_desc': 'editor'
+			},
+			title: {
+				'wp_review_desc_title' : 'text'
+			}
 		}
-    $( '#' + key ).on( 'change', function() {
-    	RankMathApp.reloadPlugin( self.pluginName );
-    });
-  });
-};
+	}
 
-rankMathCompatible.prototype.debounce = function( func, wait, immediate ) {
- var timeout;
-  return function() {
-    var context = this, args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    }
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  }
-}
+	/**
+	 * Hook into rank math app eco-system
+	 */
+	RankMathIntegration.prototype.hooks = function() {
+		RankMath.app.registerPlugin( this.pluginName, { status: 'ready' } )
+		RankMath.app.addFilter( 'content', $.proxy( this.reviewDescription, this ), this.pluginName )
+		RankMath.app.addFilter( 'title', $.proxy( this.reviewTitle, this ), this.pluginName )
+	}
 
-$( document ).on( 'ready', function () {
-  new rankMathCompatible();
-});
+	/**
+	 * Capture events from plugins to refresh rank math analysis
+	 */
+	RankMathIntegration.prototype.events = function() {
+		var self = this
+
+		$.each( self.fields.content, function( key, value ) {
+			if ( 'editor' === value && undefined !== tinyMCE.editors[ key ] ) {
+				tinyMCE.editors[ key ].on( 'change', function() {
+					RankMath.app.pluginReloaded( self.pluginName )
+				})
+			} else {
+				$( '#' + key ).on( 'change', function() {
+					RankMath.app.pluginReloaded( self.pluginName )
+				})
+			}
+		})
+	}
+
+	/**
+	 * Analyze Review fields content
+	 *
+	 * @param {String} content System content.
+	 *
+	 * @return {String} Our plugin content concatenated
+	 */
+	RankMathIntegration.prototype.reviewDescription = function( content ) {
+		return content + this.getContent()
+	}
+
+	/**
+	 * Analyze Review fields title
+	 *
+	 * @param {String} title System title.
+	 *
+	 * @return {String} Our plugin title concatenated
+	 */
+	RankMathIntegration.prototype.reviewTitle = function( title ) {
+		return title + this.getTitle()
+	}
+
+	/**
+	 * Gather content from plugin fields for analysis.
+	 *
+	 * @return {String}
+	 */
+	RankMathIntegration.prototype.getContent = function() {
+		var content = ''
+		$.each( this.fields.content, function( key, value ) {
+			content += 'editor' === value && undefined !== tinyMCE.editors[ key ] ? tinyMCE.editors[ key ].getContent() : $( '#' + key ).val()
+		})
+
+		return content
+	}
+
+	/**
+	 * Gather title from plugin fields for analysis.
+	 *
+	 * @return {String}
+	 */
+	RankMathIntegration.prototype.getTitle = function() {
+		var title = ''
+		$.each( this.fields.title, function( key, value ) {
+			title = '\n' + $( '#' + key ).val()
+		})
+
+		return title
+	}
+
+	$( window ).on( 'load', function() {
+		new RankMathIntegration()
+	})
+
+})( jQuery )
